7ab84a9c687fd973fb6ecdf72bfe23ec1b39f3b9
praction1988 <akshay.chauhan1988@gmail.com>
2025-01-28 18:40:19 +0530

    events added for user

--- events/models/domain-user/domain.user.notification.events.go
+++ events/models/domain-user/domain.user.notification.events.go
@@ -2,9 +2,37 @@ package domainUserEventModdel
 
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
-type DomainaUserAuthInitNotification struct {
-	UUID       string       `json:"uuid,omitempty"`
-	UserAccess []UserAccess `json:"userAccess"`
+type DomainUserCreateEvent struct {
+	ID             primitive.ObjectID `json:"id" bson:"_id"`
+	UUID           string             `json:"uuid,omitempty"`
+	Mobile         string             `json:"mobile"`
+	Whatsapp       string             `json:"whatsapp"`
+	Email          string             `json:"email"`
+	UserAccess     []UserAccess       `json:"userAccess"`
+	UserDepartment []UserDepartment   `json:"userDepartment"`
+	OnLeave        bool               `json:"onLeave" bson:"onLeave"`
+	IsActive       bool               `json:"isActive" bson:"isActive"`
+}
+
+type UserDepartment struct {
+	Domain     string `json:"domain"`
+	Department string `json:"department"`
+}
+
+type DomainUserUpdateEvent struct {
+	ID             primitive.ObjectID `json:"id" bson:"_id"`
+	UUID           string             `json:"uuid,omitempty"`
+	Mobile         string             `json:"mobile"`
+	Whatsapp       string             `json:"whatsapp"`
+	Email          string             `json:"email"`
+	UserAccess     []UserAccess       `json:"userAccess"`
+	UserDepartment []UserDepartment   `json:"userDepartment"`
+	OnLeave        bool               `json:"onLeave" bson:"onLeave"`
+	IsActive       bool               `json:"isActive" bson:"isActive"`
+}
+
+type DomainUserDeleteEvent struct {
+	ID primitive.ObjectID `json:"id" bson:"_id"`
 }
 
 type UserAccess struct {
@@ -12,26 +40,26 @@ type UserAccess struct {
 	Role   string `json:"role"  bson:"role" example:"550e8400-e29b-41d4-a716-446655440000"`
 }
 
-type RoleInitNotification struct {
+type RoleInitEventModel struct {
 	ID       primitive.ObjectID   `json:"id" bson:"_id"`
 	UUID     string               `json:"uuid" bson:"uuid"`
 	Policies []primitive.ObjectID `json:"policies" bson:"policies"`
 	Version  int                  `json:"version" bson:"version"`
 }
 
-type RoleUpdateNotification struct {
+type RoleUpdateEventModel struct {
 	ID       primitive.ObjectID   `json:"id" bson:"_id"`
 	UUID     string               `json:"uuid" bson:"uuid"`
 	Policies []primitive.ObjectID `json:"policies" bson:"policies"`
 	Version  int                  `json:"version" bson:"version"`
 }
 
-type RoleDeleteNotification struct {
+type RoleDeleteEvenetModel struct {
 	ID      primitive.ObjectID `json:"id" bson:"_id"`
 	Version int                `json:"version" bson:"version"`
 }
 
-type PolicyInitNotification struct {
+type PolicyInitEventModel struct {
 	ID      primitive.ObjectID `json:"id" bson:"_id"`
 	UUID    string             `json:"uuid" bson:"uuid"`
 	Object  string             `json:"object" bson:"object"`
