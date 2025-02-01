7162a76e9fb59809c8d58c054424f17ff707145c
praction1988 <akshay.chauhan1988@gmail.com>
2025-02-01 17:15:10 +0530

    domain event added

--- events/models/domain/domain.events.go
+++ events/models/domain/domain.events.go
@@ -2,7 +2,7 @@ package domainEvenetModel
 
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
-type Domain struct {
+type DomainCreate struct {
 	ID          primitive.ObjectID `json:"id" bson:"_id,omitempty"`
 	UUID        string             `json:"uuid" bson:"uuid,omitempty"`
 	Name        string             `json:"name" bson:"name,omitempty"`
@@ -11,11 +11,37 @@ type Domain struct {
 	Version     int                `json:"version" bson:"version"`
 }
 
-type Department struct {
+type DomainUpdate struct {
 	ID          primitive.ObjectID `json:"id" bson:"_id,omitempty"`
 	UUID        string             `json:"uuid" bson:"uuid,omitempty"`
 	Name        string             `json:"name" bson:"name,omitempty"`
 	SystemName  string             `json:"systemName" bson:"systemName,omitempty"`
 	ParentRefID primitive.ObjectID `json:"parentRefId,omitempty" bson:"parentRefId,omitempty"` // Nullable parent reference
 	Version     int                `json:"version" bson:"version"`
+}
+
+type DomainDelete struct {
+	ID primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+}
+
+type DepartmentCreate struct {
+	ID          primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	UUID        string             `json:"uuid" bson:"uuid,omitempty"`
+	Name        string             `json:"name" bson:"name,omitempty"`
+	SystemName  string             `json:"systemName" bson:"systemName,omitempty"`
+	ParentRefID primitive.ObjectID `json:"parentRefId,omitempty" bson:"parentRefId,omitempty"` // Nullable parent reference
+	Version     int                `json:"version" bson:"version"`
+}
+
+type DepartmentUpdate struct {
+	ID          primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	UUID        string             `json:"uuid" bson:"uuid,omitempty"`
+	Name        string             `json:"name" bson:"name,omitempty"`
+	SystemName  string             `json:"systemName" bson:"systemName,omitempty"`
+	ParentRefID primitive.ObjectID `json:"parentRefId,omitempty" bson:"parentRefId,omitempty"` // Nullable parent reference
+	Version     int                `json:"version" bson:"version"`
+}
+
+type DepartmentDelete struct {
+	ID primitive.ObjectID `json:"id" bson:"_id,omitempty"`
 }
