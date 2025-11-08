7f795fd8fff6717edbb572adf2218fa8edfdfd75
praction1988 <akshay.chauhan1988@gmail.com>
2025-11-08 13:46:02 +0530

    chore: prepare for release v1.4.25

--- events/models/tenantevent/app.messageing.event.model.go
+++ events/models/tenantevent/app.messageing.event.model.go
@@ -1,23 +1,32 @@
 package tenantevent
 
 type AppMessaegerInsertEventModel struct {
-	ID         string         `bson:"_id" json:"id"`
-	Provider   string         `bson:"provider" json:"provider"`
-	AssignedTo []string       `bson:"assignedTo" json:"assignedTo"`
-	Metadata   map[string]any `bson:"metadata,omitempty" json:"metadata,omitempty"`
-	IsActive   bool           `bson:"isActive" json:"isActive"`
-	Version    int            `bson:"version" json:"version"`
+	ID         string                 `bson:"_id" json:"id"`
+	Provider   string                 `bson:"provider" json:"provider"`
+	AssignedTo []WhatsAppTenantConfig `bson:"assignedTo" json:"assignedTo"`
+	Metadata   map[string]any         `bson:"metadata,omitempty" json:"metadata,omitempty"`
+	IsActive   bool                   `bson:"isActive" json:"isActive"`
+	Version    int                    `bson:"version" json:"version"`
 }
 
 type AppMessengerUpdateEventModel struct {
-	ID         string         `bson:"_id" json:"id"`
-	Provider   string         `bson:"provider" json:"provider"`
-	AssignedTo []string       `bson:"assignedTo" json:"assignedTo"`
-	Metadata   map[string]any `bson:"metadata,omitempty" json:"metadata,omitempty"`
-	IsActive   bool           `bson:"isActive" json:"isActive"`
-	Version    int            `bson:"version" json:"version"`
+	ID         string                 `bson:"_id" json:"id"`
+	Provider   string                 `bson:"provider" json:"provider"`
+	AssignedTo []WhatsAppTenantConfig `bson:"assignedTo" json:"assignedTo"`
+	Metadata   map[string]any         `bson:"metadata,omitempty" json:"metadata,omitempty"`
+	IsActive   bool                   `bson:"isActive" json:"isActive"`
+	Version    int                    `bson:"version" json:"version"`
 }
 
 type AppMessengerDeleteEventModel struct {
 	ID string `bson:"_id" json:"id"`
+}
+
+type WhatsAppTenantConfig struct {
+	TenantID             string   `json:"tenantID" bson:"tenantID" validate:"required"`
+	PhoneNumberID        string   `json:"phoneNumberID" bson:"phoneNumberID" validate:"required"`
+	BusinessAccountID    string   `json:"businessAccountID" bson:"businessAccountID" validate:"required"`
+	AccessToken          string   `json:"accessToken" bson:"accessToken" validate:"required"`
+	IsActive             bool     `json:"isActive" bson:"isActive"`
+	AllowedMessagesTypes []string `json:"allowedMessagesTypes,omitempty" bson:"allowedMessagesTypes,omitempty"`
 }
