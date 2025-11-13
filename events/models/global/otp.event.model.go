7507977ea0f0e05f717e3326965dbeee1064683b
praction1988 <akshay.chauhan1988@gmail.com>
2025-11-13 11:50:05 +0530

    chore: prepare for release v1.4.30

--- events/models/global/otp.event.model.go
+++ events/models/global/otp.event.model.go
@@ -8,6 +8,14 @@ type OTPEventModel struct {
 	Name           string        `json:"name,omitempty"`
 	OTP            string        `json:"otp"`
 	TenantID       string        `json:"tenant_id"`
-	CreatedAt      time.Time     `json:"created_at"`
+	ExpiryDuration time.Duration `json:"expiry_duration" default:"30m"`
+}
+
+type OTPResendEventModel struct {
+	Name           string        `json:"name"`
+	OTP            string        `json:"otp"`
+	TenantID       string        `json:"tenant_id"`
+	Resend         bool          `json:"resend" default:"false"`
+	Type           string        `json:"type" default:"sms"`
 	ExpiryDuration time.Duration `json:"expiry_duration" default:"30m"`
 }
