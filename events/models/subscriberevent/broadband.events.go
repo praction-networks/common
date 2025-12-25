ae9ccb26ab91271194b2fab305bff3cdd9188fed
Akshay Kumar <akshay.chauhan1988@gmail.com>
2025-12-25 07:35:58 +0000

    chore: prepare for release v1.4.159

---
+++ events/models/subscriberevent/broadband.events.go
@@ -0,0 +1,46 @@
+package subscriberevent
+
+// BroadbandSubscriptionCreatedEvent represents a broadband subscription creation event
+type BroadbandSubscriptionCreatedEvent struct {
+	ID                    string                      `json:"id" bson:"id"`
+	SubscriberID          string                      `json:"subscriberId" bson:"subscriberId"`
+	TenantID              string                      `json:"tenantId" bson:"tenantId"`
+	AccountNumber         string                      `json:"accountNumber" bson:"accountNumber"`
+	Status                BroadbandSubscriptionStatus `json:"status" bson:"status"`
+	ConnectionType        BroadbandConnectionType     `json:"connectionType" bson:"connectionType"`
+	AuthMethod            BroabandAuthMethod          `json:"authMethod" bson:"authMethod"`
+	AuthConfig            *BroadbandAuthConfig        `json:"authConfig,omitempty" bson:"authConfig,omitempty"` // Authentication configuration based on AuthMethod
+	StaticIPAddress       string                      `json:"staticIpAddress,omitempty" bson:"staticIpAddress,omitempty"`
+	StaticGateway         string                      `json:"staticGateway,omitempty" bson:"staticGateway,omitempty"`
+	VLANID                int                         `json:"vlanId,omitempty" bson:"vlanId,omitempty"`
+	PlanCode              string                      `json:"planCode" bson:"planCode"`
+	InstallationAddressID string                      `json:"installationAddressId,omitempty" bson:"installationAddressId,omitempty"`
+	BillingProfileID      string                      `json:"billingProfileId,omitempty" bson:"billingProfileId,omitempty"`
+	OLTDeviceID           string                      `json:"oltDeviceId,omitempty" bson:"oltDeviceId,omitempty"`
+	OLTPort               string                      `json:"oltPort,omitempty" bson:"oltPort,omitempty"`
+	CPEMetadata           BroadbandCPEMetadata         `json:"cpeMetadata,omitempty" bson:"cpeMetadata,omitempty"`
+	HistoryCPEMetadata     []BroadbandCPEMetadata     `json:"historyCpeMetadata,omitempty" bson:"historyCpeMetadata,omitempty"`
+	Notes                 string                      `json:"notes,omitempty" bson:"notes,omitempty"`
+	Version               int                         `json:"version" bson:"version"`
+}
+
+// BroadbandSubscriptionUpdatedEvent represents a broadband subscription update event
+type BroadbandSubscriptionUpdatedEvent struct {
+	ID                    string `json:"id" bson:"id"`
+	SubscriberID          string `json:"subscriberId" bson:"subscriberId"`
+	TenantID              string `json:"tenantId" bson:"tenantId"`
+	AccountNumber         string `json:"accountNumber,omitempty" bson:"accountNumber,omitempty"`
+	Status                string `json:"status,omitempty" bson:"status,omitempty"`
+	PlanCode              string `json:"planCode,omitempty" bson:"planCode,omitempty"`
+	InstallationAddressID string `json:"installationAddressId,omitempty" bson:"installationAddressId,omitempty"`
+	Version               int    `json:"version" bson:"version"`
+}
+
+// BroadbandSubscriptionDeletedEvent represents a broadband subscription deletion event
+type BroadbandSubscriptionDeletedEvent struct {
+	ID            string `json:"id" bson:"id"`
+	SubscriberID  string `json:"subscriberId" bson:"subscriberId"`
+	TenantID      string `json:"tenantId" bson:"tenantId"`
+	AccountNumber string `json:"accountNumber" bson:"accountNumber"`
+}
+
--- events/models/subscriberevent/broadband.model.go
+++ events/models/subscriberevent/broadband.model.go
@@ -1,14 +1,11 @@
 package subscriberevent
 
-type BroadbandAccessType string
+type BroadbandConnectionType string
 
 const (
-	BroadbandAccessFTTH     BroadbandAccessType = "FTTH"
-	BroadbandAccessWireless BroadbandAccessType = "WIRELESS"
-	BroadbandAccessDSL      BroadbandAccessType = "DSL"
-	BroadbandAccessCable    BroadbandAccessType = "CABLE"
-	BroadbandAccessVDSL     BroadbandAccessType = "VDSL"
-	BroadbandAccessOther    BroadbandAccessType = "OTHER"
+	BroadbandConnectionFTTH     BroadbandConnectionType = "FTTH"
+	BroadbandConnectionWireless BroadbandConnectionType = "WIRELESS"
+	BroadbandConnectionOther    BroadbandConnectionType = "OTHER"
 )
 
 type BroadbandSubscriptionStatus string
@@ -17,17 +14,71 @@ const (
 	BroadbandStatusPendingInstall BroadbandSubscriptionStatus = "PENDING_INSTALL"
 	BroadbandStatusActive         BroadbandSubscriptionStatus = "ACTIVE"
 	BroadbandStatusSuspended      BroadbandSubscriptionStatus = "SUSPENDED"
-	BroadbandStatusDisconnected   BroadbandSubscriptionStatus = "DISCONNECTED"
 	BroadbandStatusTerminated     BroadbandSubscriptionStatus = "TERMINATED"
 	BroadbandStatusChurned        BroadbandSubscriptionStatus = "CHURNED"
 	BroadbandStatusBlacklisted    BroadbandSubscriptionStatus = "BLACKLISTED"
 	BroadbandStatusGrace          BroadbandSubscriptionStatus = "GRACE"
+	BroadbandStatusExpired        BroadbandSubscriptionStatus = "EXPIRED"
+	BroadbandStatusCancelled      BroadbandSubscriptionStatus = "CANCELLED"
 )
 
-type IPAssignmentType string
+type BroabandAuthMethod string
 
 const (
-	IPAssignmentPPPoE  IPAssignmentType = "PPPOE"
-	IPAssignmentStatic IPAssignmentType = "STATIC_IP"
-	IPAssignmentDHCP   IPAssignmentType = "DHCP"
+	BroabandAuthMethodPPPoE  BroabandAuthMethod = "PPPOE"
+	BroabandAuthMethodStatic BroabandAuthMethod = "STATIC_IP"
+	BroabandAuthMethodDHCP   BroabandAuthMethod = "DHCP"
 )
+
+
+
+type BroadbandCPEMetadata struct {
+	SerialNumber        string                `json:"serialNumber,omitempty" bson:"serialNumber,omitempty"`
+	Model               string                `json:"model,omitempty" bson:"model,omitempty"`
+	Manufacturer        string                `json:"manufacturer,omitempty" bson:"manufacturer,omitempty"`
+	FirmwareVersion     string                `json:"firmwareVersion,omitempty" bson:"firmwareVersion,omitempty"`
+	SoftwareVersion     string                `json:"softwareVersion,omitempty" bson:"softwareVersion,omitempty"`
+	HardwareVersion     string                `json:"hardwareVersion,omitempty" bson:"hardwareVersion,omitempty"`
+	MACAddress          string                `json:"macAddress,omitempty" bson:"macAddress,omitempty"`
+	CPEWirelessMetadata []CPEWirelessMetadata `json:"cpeWirelessMetadata,omitempty" bson:"cpeWirelessMetadata,omitempty"`
+}
+
+type CPEWirelessMetadata struct {
+	SSID               string `json:"ssid,omitempty" bson:"ssid,omitempty"`
+	Password           string `json:"password,omitempty" bson:"password,omitempty"`
+	Security           string `json:"security,omitempty" bson:"security,omitempty"`
+	Channel            string `json:"channel,omitempty" bson:"channel,omitempty"`
+	Frequency          string `json:"frequency,omitempty" bson:"frequency,omitempty"`
+	SignalStrength     string `json:"signalStrength,omitempty" bson:"signalStrength,omitempty"`
+	SignalToNoiseRatio string `json:"signalToNoiseRatio,omitempty" bson:"signalToNoiseRatio,omitempty"`
+}
+
+// PPPoEAuthConfig contains configuration for PPPoE authentication method
+type PPPoEAuthConfig struct {
+	Username string `json:"username,omitempty" bson:"username,omitempty"` // PPPoE username
+	Password string `json:"password,omitempty" bson:"password,omitempty"` // PPPoE password
+	IP       string `json:"ip,omitempty" bson:"ip,omitempty"`           // Assigned IP address (if static)
+	MAC      string `json:"mac,omitempty" bson:"mac,omitempty"`         // MAC address for authentication
+}
+
+// StaticIPAuthConfig contains configuration for Static IP authentication method
+type StaticIPAuthConfig struct {
+	IP         string `json:"ip,omitempty" bson:"ip,omitempty"`                 // Static IP address
+	MAC        string `json:"mac,omitempty" bson:"mac,omitempty"`               // MAC address for authentication
+	Gateway    string `json:"gateway,omitempty" bson:"gateway,omitempty"`       // Gateway IP address
+	SubnetMask string `json:"subnetMask,omitempty" bson:"subnetMask,omitempty"` // Subnet mask
+	DNS       []string `json:"dns,omitempty" bson:"dns,omitempty"`             // Primary DNS server
+}
+
+// DHCPAuthConfig contains configuration for DHCP authentication method
+type DHCPAuthConfig struct {
+	MAC string `json:"mac,omitempty" bson:"mac,omitempty"` // MAC address for DHCP authentication
+}
+
+// BroadbandAuthConfig is a union type that can hold any of the three authentication config types
+// Based on the AuthMethod, only one of the fields should be populated
+type BroadbandAuthConfig struct {
+	PPPoE   *PPPoEAuthConfig   `json:"pppoe,omitempty" bson:"pppoe,omitempty"`     // PPPoE configuration (when AuthMethod is PPPOE)
+	StaticIP *StaticIPAuthConfig `json:"staticIp,omitempty" bson:"staticIp,omitempty"` // Static IP configuration (when AuthMethod is STATIC_IP)
+	DHCP    *DHCPAuthConfig    `json:"dhcp,omitempty" bson:"dhcp,omitempty"`      // DHCP configuration (when AuthMethod is DHCP)
+}
