307cf3bf3935e2f9e330ca2005c01dcddf80912e
Akshay Kumar <akshay.chauhan1988@gmail.com>
2025-12-31 06:50:42 +0000

    chore: prepare for release v1.4.173

--- events/models/subscriberevent/subscriber.events.go
+++ events/models/subscriberevent/subscriber.events.go
@@ -4,41 +4,47 @@ import "time"
 
 // SubscriberCreatedEvent represents a subscriber creation event
 type SubscriberCreatedEvent struct {
-	ID                 string                 `json:"id" bson:"id"`
-	TenantID           string                 `json:"tenantId" bson:"tenantId"`
-	DomainID           string                 `json:"domainId,omitempty" bson:"domainId,omitempty"`
-	Type               SubscriberType         `json:"type" bson:"type"`
-	Status             SubscriberStatus       `json:"status" bson:"status"`
-	ExternalRef        string                 `json:"externalRef,omitempty" bson:"externalRef,omitempty"`
-	FullName           string                 `json:"fullName,omitempty" bson:"fullName,omitempty"`
-	OrganizationName   string                 `json:"organizationName,omitempty" bson:"organizationName,omitempty"`
-	PrimaryMobile      string                 `json:"primaryMobile" bson:"primaryMobile"`
-	PrimaryEmail       string                 `json:"primaryEmail,omitempty" bson:"primaryEmail,omitempty"`
-	WhatsAppNumber     string                 `json:"whatsAppNumber,omitempty" bson:"whatsAppNumber,omitempty"`
-	PrimaryUserID      string                 `json:"primaryUserId,omitempty" bson:"primaryUserId,omitempty"`
-	MaxMacAddresses    int                    `json:"maxMacAddresses,omitempty" bson:"maxMacAddresses,omitempty"`
-	MaxSimultaneousUse int                    `json:"maxSimultaneousUse,omitempty" bson:"maxSimultaneousUse,omitempty"`
-	SubscriberFields   map[string]interface{} `json:"subscriberFields,omitempty" bson:"subscriberFields,omitempty"` // All dynamic fields from subscriber model
-	Version            int                    `json:"version" bson:"version"`
+	ID                        string                 `json:"id" bson:"id"`
+	TenantID                  string                 `json:"tenantId" bson:"tenantId"`
+	DomainID                  string                 `json:"domainId,omitempty" bson:"domainId,omitempty"`
+	Type                      SubscriberType         `json:"type" bson:"type"`
+	Status                    SubscriberStatus       `json:"status" bson:"status"`
+	BroadbandSubscriptions    []string               `bson:"broadbandSubscriptions,omitempty" json:"broadbandSubscriptions,omitempty"`
+	HotspotProfiles           []string               `bson:"hotspotProfiles,omitempty" json:"hotspotProfiles,omitempty"`
+	MaxHotspotProfiles        int                    `bson:"maxHotspotProfiles,omitempty" json:"maxHotspotProfiles,omitempty"`
+	MaxBroadbandSubscriptions int                    `bson:"maxBroadbandSubscriptions,omitempty" json:"maxBroadbandSubscriptions,omitempty"`
+	ExternalRef               string                 `json:"externalRef,omitempty" bson:"externalRef,omitempty"`
+	FullName                  string                 `json:"fullName,omitempty" bson:"fullName,omitempty"`
+	OrganizationName          string                 `json:"organizationName,omitempty" bson:"organizationName,omitempty"`
+	PrimaryMobile             string                 `json:"primaryMobile" bson:"primaryMobile"`
+	PrimaryEmail              string                 `json:"primaryEmail,omitempty" bson:"primaryEmail,omitempty"`
+	WhatsAppNumber            string                 `json:"whatsAppNumber,omitempty" bson:"whatsAppNumber,omitempty"`
+	PrimaryUserID             string                 `json:"primaryUserId,omitempty" bson:"primaryUserId,omitempty"`
+	MaxMacAddresses           int                    `json:"maxMacAddresses,omitempty" bson:"maxMacAddresses,omitempty"`
+	MaxSimultaneousUse        int                    `json:"maxSimultaneousUse,omitempty" bson:"maxSimultaneousUse,omitempty"`
+	SubscriberFields          map[string]interface{} `json:"subscriberFields,omitempty" bson:"subscriberFields,omitempty"` // All dynamic fields from subscriber model
+	Version                   int                    `json:"version" bson:"version"`
 }
 
 // SubscriberUpdatedEvent represents a subscriber update event
 type SubscriberUpdatedEvent struct {
-	ID                  string                 `json:"id" bson:"id"`
-	TenantID            string                 `json:"tenantId" bson:"tenantId"`
-	Type                SubscriberType         `json:"type,omitempty" bson:"type,omitempty"`
-	Status              SubscriberStatus       `json:"status,omitempty" bson:"status,omitempty"`
-	FullName            string                 `json:"fullName,omitempty" bson:"fullName,omitempty"`
-	OrganizationName    string                 `json:"organizationName,omitempty" bson:"organizationName,omitempty"`
-	PrimaryMobile       string                 `json:"primaryMobile,omitempty" bson:"primaryMobile,omitempty"`
-	PrimaryEmail        string                 `json:"primaryEmail,omitempty" bson:"primaryEmail,omitempty"`
-	WhatsAppNumber      string                 `json:"whatsAppNumber,omitempty" bson:"whatsAppNumber,omitempty"`
-	MaxMacAddresses     int                    `json:"maxMacAddresses,omitempty" bson:"maxMacAddresses,omitempty"`
-	MaxSimultaneousUse  int                    `json:"maxSimultaneousUse,omitempty" bson:"maxSimultaneousUse,omitempty"`
-	SubscriberFields    map[string]interface{} `json:"subscriberFields,omitempty" bson:"subscriberFields,omitempty"`       // All dynamic fields from subscriber model
-	FirstHotspotLoginAt *time.Time             `json:"firstHotspotLoginAt,omitempty" bson:"firstHotspotLoginAt,omitempty"` // When subscriber first logged into hotspot
-	LastHotspotLoginAt  *time.Time             `json:"lastHotspotLoginAt,omitempty" bson:"lastHotspotLoginAt,omitempty"`   // When subscriber last logged into hotspot
-	Version             int                    `json:"version" bson:"version"`
+	ID                        string                 `json:"id" bson:"id"`
+	TenantID                  string                 `json:"tenantId" bson:"tenantId"`
+	Type                      SubscriberType         `json:"type,omitempty" bson:"type,omitempty"`
+	Status                    SubscriberStatus       `json:"status,omitempty" bson:"status,omitempty"`
+	BroadbandSubscriptions    []string               `bson:"broadbandSubscriptions,omitempty" json:"broadbandSubscriptions,omitempty"`
+	HotspotProfiles           []string               `bson:"hotspotProfiles,omitempty" json:"hotspotProfiles,omitempty"`
+	MaxHotspotProfiles        int                    `bson:"maxHotspotProfiles,omitempty" json:"maxHotspotProfiles,omitempty"`
+	MaxBroadbandSubscriptions int                    `bson:"maxBroadbandSubscriptions,omitempty" json:"maxBroadbandSubscriptions,omitempty"`
+	FullName                  string                 `json:"fullName,omitempty" bson:"fullName,omitempty"`
+	OrganizationName          string                 `json:"organizationName,omitempty" bson:"organizationName,omitempty"`
+	PrimaryMobile             string                 `json:"primaryMobile,omitempty" bson:"primaryMobile,omitempty"`
+	PrimaryEmail              string                 `json:"primaryEmail,omitempty" bson:"primaryEmail,omitempty"`
+	WhatsAppNumber            string                 `json:"whatsAppNumber,omitempty" bson:"whatsAppNumber,omitempty"`
+	SubscriberFields          map[string]interface{} `json:"subscriberFields,omitempty" bson:"subscriberFields,omitempty"`       // All dynamic fields from subscriber model
+	FirstHotspotLoginAt       *time.Time             `json:"firstHotspotLoginAt,omitempty" bson:"firstHotspotLoginAt,omitempty"` // When subscriber first logged into hotspot
+	LastHotspotLoginAt        *time.Time             `json:"lastHotspotLoginAt,omitempty" bson:"lastHotspotLoginAt,omitempty"`   // When subscriber last logged into hotspot
+	Version                   int                    `json:"version" bson:"version"`
 }
 
 // SubscriberDeletedEvent represents a subscriber deletion event
