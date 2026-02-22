2d2fcc1245ca3efe2ce4237e82c02635a3a0fa22
Akshay Kumar <akshay.chauhan1988@gmail.com>
2026-02-22 10:17:16 +0000

    chore: prepare for release v1.4.254

--- events/models/inventoryevent/device.event.model.go
+++ events/models/inventoryevent/device.event.model.go
@@ -5,3 +5,13 @@ type DeviceInsertEventModel struct {
 	Metadata map[string]any `bson:"metadata" json:"metadata"`
 	IsActive bool           `bson:"isActive" json:"isActive"`
 }
+
+type DeviceUpdateEventModel struct {
+	ID       string         `bson:"_id" json:"id"`
+	Metadata map[string]any `bson:"metadata" json:"metadata"`
+	IsActive bool           `bson:"isActive" json:"isActive"`
+}
+
+type DeviceDeleteEventModel struct {
+	ID string `bson:"_id" json:"id"`
+}
