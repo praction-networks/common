b4059f436f1b5dae065d0241f465dc52d18e1ce0
praction1988 <praction1988@users.noreply.github.com>
2026-04-18 20:25:28 +0530

    chore: prepare for release v1.4.333

--- events/models/tenantevent/sms.provider.event.model.go
+++ events/models/tenantevent/sms.provider.event.model.go
@@ -27,11 +27,11 @@ type TenantSMSProviderUpdateEventModel struct {
 	Scope             string                   `bson:"scope,omitempty" json:"scope,omitempty"`
 	ExplicitTenantIDs []string                 `bson:"explicitTenantIds,omitempty" json:"explicitTenantIds,omitempty"`
 	TenantConfig      *TenantSMSProviderConfig `bson:"tenantConfig,omitempty" json:"tenantConfig,omitempty"`
-	Priority          int                      `bson:"priority,omitempty" json:"priority,omitempty"`
+	Priority          *int                     `bson:"priority,omitempty" json:"priority,omitempty"`
 	IsActive          *bool                    `bson:"isActive,omitempty" json:"isActive,omitempty"`
 	FailoverOn        *bool                    `bson:"failoverOn,omitempty" json:"failoverOn,omitempty"`
-	MaxRetries        int                      `bson:"maxRetries,omitempty" json:"maxRetries,omitempty"`
-	Weight            int                      `bson:"weight,omitempty" json:"weight,omitempty"`
+	MaxRetries        *int                     `bson:"maxRetries,omitempty" json:"maxRetries,omitempty"`
+	Weight            *int                     `bson:"weight,omitempty" json:"weight,omitempty"`
 	Version           int                      `bson:"version" json:"version"`
 }
 
